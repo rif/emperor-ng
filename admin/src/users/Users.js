fecad8103a5e7941401322a21c5a862ec6e51864
Radu Ioan Fericean <radu@fericean.ro>
2021-02-09 02:05:21 +0200

    Add create user dialog

--- admin/src/users/Users.js
+++ admin/src/users/Users.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
+import FormDialog from './CreateUserDialog';
 
 const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'email', headerName: 'Email', width: 130 },
+    { field: 'email', headerName: 'Email', width: 200 },
     { field: 'firstName', headerName: 'FirstName', width: 130 },
     { field: 'lastName', headerName: 'LastName', width: 130 },
-    { field: 'phone', headerName: 'Phone', width: 90},
+    { field: 'phone', headerName: 'Phone', width: 120},
     {
         field: 'fullName',
         headerName: 'Full name',
@@ -25,13 +26,16 @@ export default function Users() {
             .then(response => response.json())
             .then((json) => {
                 setUsers(json.items);
-                console.log(users);
             });
     }, []);
     return (
-            <div style={{ height: 400, width: '100%' }}>
-            <DataGrid rows={users} columns={columns} pageSize={5} checkboxSelection />
+        <div>
+            <div>
+                <FormDialog/>
             </div>
+            <div style={{ height: 800, width: '100%' }}>
+                <DataGrid rows={users} columns={columns} pageSize={5} checkboxSelection />
+            </div>
+        </div>
     );
 }
-
